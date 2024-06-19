843359512f0168ccb77589a6e418286e161c7e56
Udai Chauhan <udaichauhan284@gmail.com>
2024-06-19 23:45:28 +0530

    Added ques in DP and LC POTD in BinarySearch miniNumberofBouquets

--- A2Z DSA Striver/Binary Search/MiniNumOfDMakeMBouqets.js
+++ A2Z DSA Striver/Binary Search/MiniNumOfDMakeMBouqets.js
@@ -8,12 +8,12 @@ function possible(arr, day, m, k) {
   let noOfB = 0;
   // Count the number of bouquets
   for (let i = 0; i < n; i++) {
-      if (arr[i] <= day) {
-          cnt++;
-      } else {
-          noOfB += Math.floor(cnt / k);
-          cnt = 0;
-      }
+    if (arr[i] <= day) {
+      cnt++;
+    } else {
+      noOfB += Math.floor(cnt / k);
+      cnt = 0;
+    }
   }
   noOfB += Math.floor(cnt / k);
   return noOfB >= m;
@@ -24,15 +24,15 @@ function roseGarden(arr, k, m) {
   let n = arr.length; // Size of the array
   if (val > n) return -1; // Impossible case
   // Find maximum and minimum
-  let mini = Infinity, maxi = -Infinity;
+  let mini = Infinity,
+    maxi = -Infinity;
   for (let i = 0; i < n; i++) {
-      mini = Math.min(mini, arr[i]);
-      maxi = Math.max(maxi, arr[i]);
+    mini = Math.min(mini, arr[i]);
+    maxi = Math.max(maxi, arr[i]);
   }
 
   for (let i = mini; i <= maxi; i++) {
-      if (possible(arr, i, m, k))
-          return i;
+    if (possible(arr, i, m, k)) return i;
   }
   return -1;
 }
@@ -48,39 +48,149 @@ if (ans === -1) {
 }
 
 //Optimal Way
-var minDays = function(bloomDay, m, k) {
-  function possible(bloomDay,day,numFlower){
+var minDays = function (bloomDay, m, k) {
+  function possible(bloomDay, day, numFlower) {
     let noOfB = 0;
     let count = 0;
-    let len=bloomDay.length;
-    for(let i=0; i<len; i++){
-      if(bloomDay[i] <= day){
+    let len = bloomDay.length;
+    for (let i = 0; i < len; i++) {
+      if (bloomDay[i] <= day) {
         count++;
-      }else{
+      } else {
         noOfB += Math.floor(count / numFlower);
-        count =0;
+        count = 0;
       }
     }
     noOfB += Math.floor(count / numFlower);
     return noOfB;
-  };
+  }
 
   let len = bloomDay.length;
-  let val = m*k;
-  if(val > len) return -1;
+  let val = m * k;
+  if (val > len) return -1;
 
   let low = Math.min(...bloomDay);
   let high = Math.max(...bloomDay);
   let ans = -1;
 
-  while(low<=high){
-    let mid = Math.floor((low+high)/2);
-    if(possible(bloomDay,mid,k) >= m){
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (possible(bloomDay, mid, k) >= m) {
       ans = mid;
       high = mid - 1;
-    }else{
-      low=mid+1;
+    } else {
+      low = mid + 1;
     }
   }
   return ans;
 };
+
+//This is good Approach
+//19 June 2024 - Leetcode POTD, Array, binary Search
+/* In this we want mini days to take bloom flower so that
+we can make bouquets. so do Binary Search on Answer
+TC: O(nlog(maxDay)), SC: O(1)
+*/
+var minDays = function (bloomDay, m, k) {
+  let len = bloomDay.length;
+  //binary search
+  let startDay = 0;
+  //for end day we need max value
+  let maxDay = bloomDay[0];
+  for (let i = 0; i < len; i++) {
+    //O(n)
+    if (bloomDay[i] > maxDay) {
+      maxDay = bloomDay[i];
+    }
+  }
+  let endDay = maxDay;
+  let minDay = -1;
+  if (m * k > len) return -1;
+
+  //TC: O(nlog(maxDay))
+  while (startDay <= endDay) {
+    //O(log(maxDay))
+    let mid = startDay + Math.floor((endDay - startDay) / 2);
+
+    if (canMakeBouquets(bloomDay, mid, k) >= m) {
+      //O(n)
+      minDay = mid;
+      //need more mini day so
+      endDay = mid - 1;
+    } else {
+      startDay = mid + 1;
+    }
+  }
+  return minDay;
+};
+//canMakeBouquets function to see how many bouquets we can make
+//for k adjecent flower
+function canMakeBouquets(arr, mid, k) {
+  let numOfBou = 0;
+  let consecutiveDays = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] <= mid) {
+      consecutiveDays++;
+    } else {
+      consecutiveDays = 0;
+    }
+
+    if (consecutiveDays === k) {
+      numOfBou++;
+      //again make it zero for consecutive days
+      consecutiveDays = 0;
+    }
+  }
+  return numOfBou;
+}
+
+//19 June 2024,
+/* Brute Method, for loop from mini of bloomDay to max of BloomDay
+TC: O(n*n)+O(n), SC: O(1)
+This will give TLE. 
+*/
+var minDays = function (bloomDay, m, k) {
+  let len = bloomDay.length;
+  let val = m * k;
+  let minDay = -1;
+  if (val > len) return minDay;
+  let mini = Number.MAX_VALUE;
+  let maxi = Number.MIN_VALUE;
+  for (let i = 0; i < len; i++) {
+    if (bloomDay[i] > maxi) {
+      maxi = bloomDay[i];
+    }
+    if (bloomDay[i] < mini) {
+      mini = bloomDay[i];
+    }
+  }
+
+  //for loop from mini and maxi
+  for (let i = mini; i <= maxi; i++) {
+    if (canMakeBouquets(bloomDay, i, k) >= m) {
+      miniDay = i;
+      break;
+    }
+  }
+  return miniDay;
+};
+//canMakeBouquets function to see how many bouquets we can make
+//for k adjecent flower
+function canMakeBouquets(arr, day, k) {
+  let numOfBou = 0;
+  let consecutiveDays = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] <= day) {
+      consecutiveDays++;
+    } else {
+      consecutiveDays = 0;
+    }
+
+    if (consecutiveDays === k) {
+      numOfBou++;
+      //again make it zero for consecutive days
+      consecutiveDays = 0;
+    }
+  }
+  return numOfBou;
+}
---
+++ A2Z DSA Striver/Dynamic Programming/DP Subsequences/416PartitionEqualSubsetSum.js
@@ -0,0 +1,45 @@
+/* 416. Partition Equal Subset Sum
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+*/
+/*We need to divide the subset into two part to check the 
+condition, so if total sum is S, so s1===s2=== (s/2)
+if s is odd, we cant divide in two equal part, so return false
+here we can use subset sum equal to k, and k will be sum/2
+TC: O(n*target), SC: O(n), we will use space optimization one
+*/
+var canPartition = function (nums) {
+  let n = nums.length;
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += nums[i];
+  }
+  if (sum % 2 === 1) return false; //odd sum, cant divide into two/
+  let target = Math.floor(sum / 2);
+  return subsetSumEqualToK(nums, n, target);
+};
+function subsetSumEqualToK(nums, n, target) {
+  let prev = Array(target + 1).fill(false);
+  //base case
+  prev[0] = true;
+  if (nums[0] <= target) {
+    prev[nums[0]] = true;
+  }
+  //movement start
+  for (let i = 1; i < n; i++) {
+    let curr = Array(target + 1).fill(false);
+    for (let k = 1; k <= target; k++) {
+      let notTake = prev[k];
+      let take = false;
+      if (nums[i] <= k) {
+        take = prev[k - nums[i]];
+      } else {
+        tale = false;
+      }
+      curr[k] = notTake || take;
+    }
+    prev = curr;
+  }
+  return prev[target];
+}
