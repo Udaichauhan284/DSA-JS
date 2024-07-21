fec13c253abd88bbd58617715bf0bd3bf6380962
Udai Chauhan <udaichauhan284@gmail.com>
2024-07-22 00:07:31 +0530

    in graph LC POTD

--- A2Z DSA Striver/Graphs/2392BuildaMatrixWithCondtions.js
+++ A2Z DSA Striver/Graphs/2392BuildaMatrixWithCondtions.js
@@ -11,3 +11,73 @@ The column conditions are the following:
 - Number 3 is in column 0, and number 2 is in column 1, so 3 is left of 2 in the matrix.
 Note that there may be multiple correct answers.
 */
+
+//TC: O(k^2)+O(k)+O(k) ~ O(k^2), SC: O(k)
+var buildMatrix = function (k, rowConditions, colConditions) {
+  let orderRows = topoSort(rowConditions, k);
+  let orderColumns = topoSort(colConditions, k);
+
+  // We might have found cycle. That's why topo order is empty
+  if (orderRows.length === 0 || orderColumns.length === 0)
+      return [];
+
+  let matrix = Array.from({ length: k }, () => Array(k).fill(0));
+
+  for (let i = 0; i < k; i++) {
+      for (let j = 0; j < k; j++) {
+          if (orderRows[i] === orderColumns[j]) {
+              matrix[i][j] = orderRows[i];
+          }
+      }
+  }
+
+  return matrix;
+};
+function topoSort(edges, n) {
+  let adj = new Map();
+  let st = [];
+  let order = [];
+  // 0 : not visited
+  // 1 : visiting (currently in stack via some other DFS)
+  // 2 : visited
+  let visited = Array(n + 1).fill(0);
+  let hasCycle = { value: false };
+
+  for (let edge of edges) {
+      let u = edge[0];
+      let v = edge[1];
+      if (!adj.has(u)) {
+          adj.set(u, []);
+      }
+      adj.get(u).push(v);
+  }
+
+  for (let i = 1; i <= n; i++) {
+      if (visited[i] === 0) {
+          dfs(i, adj, visited, st, hasCycle);
+          if (hasCycle.value)
+              return []; // no ordering possible
+      }
+  }
+
+  while (st.length > 0) {
+      order.push(st.pop());
+  }
+  return order;
+}
+function dfs(node, adj, visited, st, hasCycle) {
+  visited[node] = 1; // Mark node as visiting
+  // First, visit node's children
+  for (let nbr of (adj.get(node) || [])) {
+      if (visited[nbr] === 0) {
+          dfs(nbr, adj, visited, st, hasCycle);
+      } else if (visited[nbr] === 1) {
+          // Cycle detected
+          hasCycle.value = true;
+          return;
+      }
+  }
+
+  visited[node] = 2; // Mark node as visited
+  st.push(node); // Now node can be added
+}
