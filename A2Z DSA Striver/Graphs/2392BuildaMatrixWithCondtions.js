7bb087a63e8037ec480a08ef48822c38314e51e0
Udai Chauhan <udaichauhan284@gmail.com>
2024-07-21 23:57:34 +0530

    Added LC POTD in graph

---
+++ A2Z DSA Striver/Graphs/2392BuildaMatrixWithCondtions.js
@@ -0,0 +1,13 @@
+/* 2392. Build a Matrix With Conditions
+
+Input: k = 3, rowConditions = [[1,2],[3,2]], colConditions = [[2,1],[3,2]]
+Output: [[3,0,0],[0,0,1],[0,2,0]]
+Explanation: The diagram above shows a valid example of a matrix that satisfies all the conditions.
+The row conditions are the following:
+- Number 1 is in row 1, and number 2 is in row 2, so 1 is above 2 in the matrix.
+- Number 3 is in row 0, and number 2 is in row 2, so 3 is above 2 in the matrix.
+The column conditions are the following:
+- Number 2 is in column 1, and number 1 is in column 2, so 2 is left of 1 in the matrix.
+- Number 3 is in column 0, and number 2 is in column 1, so 3 is left of 2 in the matrix.
+Note that there may be multiple correct answers.
+*/
