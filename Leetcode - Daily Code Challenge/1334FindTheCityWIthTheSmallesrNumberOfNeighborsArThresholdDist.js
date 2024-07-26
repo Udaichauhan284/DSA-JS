2bec2ba8df3889133c80b9a621263b7bc920cc06
Udai Chauhan <udaichauhan284@gmail.com>
2024-07-26 19:07:04 +0530

    Added todays LC POTD in Leetcode Daily section, rated to graph question 1334

--- Leetcode - Daily Code Challenge/1334FindTheCityWIthTheSmallesrNumberOfNeighborsArThresholdDist.js
+++ Leetcode - Daily Code Challenge/1334FindTheCityWIthTheSmallesrNumberOfNeighborsArThresholdDist.js
@@ -22,8 +22,112 @@ point, but can be solve using Dij Algo, use of MinHeap
 Dijkstra Algo TC: O(V + ElogE), so overavll O(V.(V+ElogE)) ~ O(n.(n+ElogE))
 SC: O(n^2 + n + e)
 */
+class MinHeap{
+  constructor(){
+    this.data = [];
+  }
+  getParentIndex(idx){
+    return Math.floor((idx-1)/2);
+  }
+  getLeftChildIndex(idx){
+    return 2 * idx + 1;
+  }
+  getRightChildIndex(idx){
+    return 2 * idx + 2;
+  }
+  swap(i1, i2){
+    [this.data[i1], this.data[i2]] = [this.data[i2], this.data[i1]];
+  }
+  push(pair){
+    this.data.push(pair);
+    this.heapifyUp();
+  }
+  heapifyUp(){
+    let idx = this.data.length - 1;
+    while(idx > 0){
+      let parenIndex = this.getParentIndex(idx);
+      if(this.data[idx][0] < this.data[parenIndex][0]){
+        this.swap(idx, parenIndex);
+        idx = parenIndex;
+      }else{
+        break;
+      }
+    }
+  }
+  poll(){
+    if(this.data.length === 0) return null;
+    if(this.data.length === 1) return this.data.pop();
+    let minValue = this.data[0];
+    this.data[0] = this.data.pop();
+    this.heapifyDown(0);
+    return minValue;
+  }
+  heapifyDown(idx){
+    while(true){
+      let smallest = idx;
+      let left = this.getLeftChildIndex(idx);
+      let right = this.getRightChildIndex(idx);
+      if(left < this.data.length && this.data[left][0] < this.data[smallest][0]){
+        smallest = left;
+      }
+      if(right < this.data.length && this.data[right][0] < this.data[samllest][0]){
+        smallest = right;
+      }
+      if(smallest !== idx){
+        this.swap(idx, smallest);
+        idx = smallest;
+      }else{
+        break;
+      }
+    }
+  }
+  isEmpty(){
+    return this.data.length === 0;
+  }
+}
 const findTheCity1 = (n, edges, distanceThreshold) => {
+  //for dij algo we need adj graph
+  let adj = Array.from({length : n}, () => []);
+  for(let [u,v,wt] of edges){
+    adj[u].push([v,wt]);
+    adj[v].push([u,wt]);
+  }
 
+  //main code
+  let minReachableCities = n;
+  let resultCity = -1;
+  for(let i=0; i<n; i++){
+    //here dij algo start
+    let dist = Array(n).fill(Number.MAX_VALUE);
+    dist[i] = 0;
+    let pq = new MinHeap();
+    pq.push([0,i]); //dist, source node
+
+    while(!pq.isEmpty()){
+      let [currDist, node] = pq.poll();
+      if(currDist > dist[node]) continue;
+
+      for(let [nextNode, wt] of adj[node]){
+        let nextDist = currDist + wt;
+        if(nextDist < dist[nextNode]){
+          dist[nextNode] = nextDist;
+          pq.push([nextDist, nextNode]);
+        }
+      }
+    }
+
+    let reachableCity = 0;
+    for(let city=0; city<n; city++){
+      if(dist[city] <= distanceThreshold){
+        reachableCity++;
+      }
+    }
+    if(reachableCity <= minReachableCities){
+      minReachableCities = reachableCity;
+      resultCity = i;
+    }
+  }
+  return resultCity;
 }
 
 
