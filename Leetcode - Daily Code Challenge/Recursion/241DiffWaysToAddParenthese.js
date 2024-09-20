6d36bfaa3f0851a397d045c2d49201a857110f0a
Udai Chauhan <udaichauhan284@gmail.com>
2024-09-20 21:21:33 +0530

    Update DP version of yesterday LC POTD in recursion section

--- Leetcode - Daily Code Challenge/Recursion/241DiffWaysToAddParenthese.js
+++ Leetcode - Daily Code Challenge/Recursion/241DiffWaysToAddParenthese.js
@@ -39,3 +39,40 @@ var diffWaysToCompute = function(expression) {
   }
   return result;
 };
+
+/*Method 2 this can also be solved by using DP
+in solve function pass start and end and also DP array
+*/
+var diffWaysToCompute = function(expression) {
+  let len = expression.length;
+  let dp = Array.from({length : len}, () => Array(len).fill(-1));
+  return solve(expression, 0, expression.length-1, dp);
+};
+function solve(exp,start,end,dp){
+  let result = [];
+  if(dp[start][end] !== -1) return dp[start][end];
+  //first we need to check if the exp have operators
+  for(let i=start; i<=end; i++){
+      if(exp[i] === "+" || exp[i] === "-" || exp[i] === "*"){
+          let left = solve(exp, start, i-1, dp);
+          let right = solve(exp, i+1, end, dp);
+
+          for(let x of left){
+              for(let y of right){
+                  if(exp[i] === "+"){
+                      result.push(x+y);
+                  }else if(exp[i] === "-"){
+                      result.push(x-y);
+                  }else {
+                      result.push(x*y);
+                  }
+              }
+          }
+      }
+  }
+  if(result.length === 0){
+      result.push(parseInt(exp.substr(start)));
+  }
+  dp[start][end] = result;
+  return dp[start][end];
+}
