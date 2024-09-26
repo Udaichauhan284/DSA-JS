886d9b10ab7078fec08388400ad16e535a53ef77
Udai Chauhan <udaichauhan284@gmail.com>
2024-09-26 19:21:45 +0530

    update of 238Productofarrayexceptitself in leetcode daily array medium

--- Leetcode - Daily Code Challenge/Arrays/Medium/238ProductOfArrExceptSelf.js
+++ Leetcode - Daily Code Challenge/Arrays/Medium/238ProductOfArrExceptSelf.js
@@ -74,3 +74,77 @@ const productExceptSelf = (nums) => {
   }
   return result;
 }
+
+
+/*26 Sept 2024, Brute Method
+use of nested for loop, when i !==j then we calc
+the product of i and j elem in array
+TC: O(n^2), SC: O(1)
+TLE
+*/
+var productExceptSelf1 = function(nums) {
+  let len = nums.length;
+  let ans = Array(len).fill(1);
+  for(let i=0; i<len; i++){
+      for(let j=0; j<len; j++){
+          if(i !== j){
+              ans[i] *= nums[j];
+          }
+      }
+  }
+  return ans
+};
+
+/*AFter brute method, Optimal 1, we can take left and
+right arr, for num's left value multiplication and
+num's right value multiplication, and then. in main
+loop we multiple both left and right value together
+and form the ans TC: O(3n) ~ O(n), SC: O(n)
+*/
+const productExceptSelf2 = (nums) => {
+  let len = nums.length;
+  let prefix = Array(len).fill(1); //left arr
+  let suffix = Array(len).fill(1); //right arr
+  let ans = Array(len).fill(1); // main ans arr
+  //now calculate prefix multiple for each num
+  for(let i=1; i<len; i++){
+      //we start from 1, because we have already 1 at zero index
+      prefix[i] = prefix[i-1]*nums[i-1];
+  }
+
+  //now calculate suffix multiple for each num
+  //in this we start from right to left, as we need to stor emultiple of right side
+  for(let j=len-2; j>=0; j--){
+      suffix[j] = suffix[j+1] * nums[j+1];
+  }
+
+  //now multiple both left and right
+  for(let i=0; i<len; i++){
+      ans[i] = prefix[i] * suffix[i];
+  }
+  return ans;
+}
+
+/*26 Sept 2024, Method3- optimal method, instead use 
+of prefix(left) and suffix(Right) arr, to calculate
+in two different loop and then multiple, instead, we
+can directly multiple in ans for prefix and for suffix
+we need to take extra varibale and it worl
+TC: O(n), SC: O(1)
+*/
+var productExceptSelf = function(nums) {
+  let len = nums.length;
+  let ans = Array(len).fill(1);
+
+  //now calulate the prefix in ans only
+  for(let i=1; i<len; i++){
+      ans[i] = ans[i-1] * nums[i-1]; //1 * 1
+  }
+  //now calculate for suffix 
+  let suffix = 1;
+  for(let i=len-2; i>=0; i--){
+      suffix *= nums[i+1];
+      ans[i] *= suffix;
+  }
+  return ans;
+};
