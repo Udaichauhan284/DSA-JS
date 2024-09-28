429a2956c5d85938b266616d7ddbe68df0f53e0e
Udai Chauhan <udaichauhan284@gmail.com>
2024-09-28 16:08:20 +0530

    udpate in Isomorphic question

--- A2Z DSA Striver/String/Isomorphic.js
+++ A2Z DSA Striver/String/Isomorphic.js
@@ -14,3 +14,24 @@
   }
   return true;
 };
+
+class Solution {
+  isomorphicString(s, t) {
+      //your code goes here
+      //this can also solve using array of ASCII code till 256
+      let m1 = Array(256).fill(0);
+      let m2 = Array(256).fill(0);
+      if(t.length > s.length){
+          return false;
+      }
+      for(let i=0; i<s.length; i++){
+          if(m1[s.charCodeAt(i)] !== m2[t.charCodeAt(i)]){
+              return false;
+          }
+          //now add it
+          m1[s.charCodeAt(i)] = i+1;
+          m2[t.charCodeAt(i)] = i+1;
+      }
+      return true;
+  }
+}
