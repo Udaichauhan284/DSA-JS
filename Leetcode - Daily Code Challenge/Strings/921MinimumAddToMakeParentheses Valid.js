b220a0e789e6167664352addde7fc10c8961fc51
Udai Chauhan <udaichauhan284@gmail.com>
2024-10-09 23:12:22 +0530

    Add some update in LC 921

--- Leetcode - Daily Code Challenge/Strings/921MinimumAddToMakeParentheses Valid.js
+++ Leetcode - Daily Code Challenge/Strings/921MinimumAddToMakeParentheses Valid.js
@@ -31,3 +31,32 @@ var minAddToMakeValid = function(s) {
   //at last we will have odd len parenthese, which means we need to add that much parenthese
   return open+stack.length;
 };
+
+
+
+
+ /*09 Oct 2024, LC POTD
+ method 2- this also solve withour using stack, in prev
+ method we just increasing the size of stack, on that we
+ can take variable. TC O(n), SC: O(1)
+ */
+ var minAddToMakeValid = function(s) {
+  let size = 0;
+  let open = 0; //this is odd one
+  for(let ch of s){
+      if(ch === "("){
+          size++;
+      }else if(size > 0){
+          //if there is size means there is open bracket
+          //so if we see closing one, and size > 0, pop it out 
+          //means decrease the size
+          size--;
+      }else{
+          //if not open one and not size > 0, measn we get close one, for 
+          //that we need to increase the size of open bracket, so that make
+          //it even,
+          open++;
+      }
+  }
+  return size+open;
+};
