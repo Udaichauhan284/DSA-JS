8236e957c6495dcb1e7d35bc3ca905f5115a2202
Udai Chauhan <udaichauhan284@gmail.com>
2024-11-23 23:08:35 +0530

    Add LC POTD in Array medium

---
+++ Leetcode - Daily Code Challenge/Arrays/Medium/1861RotatingTheBox.js
@@ -0,0 +1,52 @@
+/*1861. Rotating The Box
+23 Nov 2024, Leetcode POTD, array, Matrix
+
+Input: box = [["#",".","#"]]
+Output: [["."],
+         ["#"],
+         ["#"]]
+*/
+
+/*Method 1-first use Transpose and then reverse the row
+so that we get the 90deg of box. then apply gravity on
+box.
+TC: O(col * row * row)
+*/
+var rotateTheBox = function (box) {
+  let m = box.length;
+  let n = box[0].length;
+
+  let result = Array.from({ length: n }, () => Array(m));
+
+  //transpose
+  for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+          result[i][j] = box[j][i];
+      }
+  }
+  //now reverse the row for 90deg rotate
+  for (let row of result) {
+      row.reverse();
+  }
+  //now apply gravity
+  for (let j = 0; j < m; j++) {
+      for (let i = n - 1; i >= 0; i--) {
+          if (result[i][j] === '.') {
+              let stoneRow = -1;
+              for (let k = i - 1; k >= 0; k--) {
+                  if (result[k][j] == '*') {
+                      break;
+                  } else if (result[k][j] == '#') {
+                      stoneRow = k;
+                      break;
+                  }
+              }
+              if(stoneRow !== -1){
+                  result[i][j] = "#";
+                  result[stoneRow][j] = ".";
+              }
+          }
+      }
+  }
+  return result;
+};
