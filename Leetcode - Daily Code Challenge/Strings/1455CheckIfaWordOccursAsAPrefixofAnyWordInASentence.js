e153aa9a6f251a5de8d20f442a2413cbf04dead7
Udai Chauhan <udaichauhan284@gmail.com>
2024-12-02 22:37:30 +0530

    Update in LC POTD 1455 in string section

--- Leetcode - Daily Code Challenge/Strings/1455CheckIfaWordOccursAsAPrefixofAnyWordInASentence.js
+++ Leetcode - Daily Code Challenge/Strings/1455CheckIfaWordOccursAsAPrefixofAnyWordInASentence.js
@@ -17,3 +17,36 @@ var isPrefixOfWord = function(sentence, searchWord) {
   }
   return -1;
 };
+
+/*Method2-without the use of inbuilt function
+we use the two pointer approach
+TC: TC: O(n), SC: O(1)
+*/
+const isPrefixOfWord1 = (sentence, searchWord) => {
+  let senLen = sentence.length;
+  let swLen = searchWord.length;
+  let i=0; // for sentence pointer
+  let index = 1; //this return the ans
+  while(i < senLen){
+    //first check the leading spaces
+    while(i < senLen && sentence.charAt(i) === " ") i++;
+
+    //now start comparing the sentence and search word using two pointer
+    let j = 0; //for searchword
+    while(i < senLen && j < swLen && sentence.charAt(i) === searchWord.charAt(j)){
+      i++;
+      j++;
+    }
+    //now checj j is done or not
+    if(j === swLen){
+      return index;
+    }
+    //now if not j done, move to next word of sentence
+    while(i<senLen && sentence.charAt(i) !== " ") i++;
+
+    //now move to next word
+    i += 1;
+    index++;
+  }
+  return -1;
+}
