17872a6c123bd3036ce20dc0c6ea13e46ec9b06e
Udai Chauhan <udaichauhan284@gmail.com>
2024-12-31 18:21:50 +0530

    ADD PowerSet of String good method in A2Z Recursion

--- A2Z DSA Striver/Recursion/PowerSetOfStr.js
+++ A2Z DSA Striver/Recursion/PowerSetOfStr.js
@@ -19,3 +19,32 @@ let curr = "";
 let result = [];
 powerSet(str,index,curr,result);
 console.log(result);
+
+
+//Best One - 31 Dec 2024
+class Solution {
+  //TC: O(2^n * n) for every index we have two option, then for pushing into result n
+  //SC: O(n)
+  AllPossibleStrings(s){
+      //code here
+      let result = [];
+      let curr = [];
+      this.solve(curr, 0, s, result);
+      result.sort();
+      return result;
+  }
+  solve(curr, idx, s, result){
+      //base case
+      if(idx === s.length){
+          if(curr.length !== 0)
+          result.push(curr.join(""));
+          return;
+      }
+      
+      //now we have option to take it and explore and then pop it out
+      curr.push(s[idx]);
+      this.solve(curr, idx+1, s, result);
+      curr.pop();
+      this.solve(curr, idx+1, s, result); //explore one more time, we dont take it
+  }
+}
