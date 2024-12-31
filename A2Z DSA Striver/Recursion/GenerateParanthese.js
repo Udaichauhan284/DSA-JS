00d27d706bfe05cd967d6258370f22f35c6f06ae
Udai Chauhan <udaichauhan284@gmail.com>
2024-12-31 14:50:36 +0530

    ADD 22 generate parentheses brute method in a2z recursion

--- A2Z DSA Striver/Recursion/GenerateParanthese.js
+++ A2Z DSA Striver/Recursion/GenerateParanthese.js
@@ -26,3 +26,52 @@ function solve(n,left,right,result,temp){
 }
 let n = 3;
 console.log(generateParanthese(n));
+
+
+/*31 Dec 2024, use of Backtracking, as we have option
+to push the open one and close one, in method1 we 
+take the curr string and in that we add and pop for 
+both bracket, and then we check isValid or not.
+TC: O(2^2n * 2n) n=3, but with both open and close 2n
+and 2n is for checking the valid curr
+SC: O(2n)
+*/
+var generateParanthese1 = (n) => {
+  let result = [];
+  let curr = [];
+  solve(curr, n, result);
+  return result;
+}
+function solve(curr, n, result){
+  //base case
+  if(curr.length === 2*n){
+    if(isValid(curr)){
+      result.push(curr.join(""));
+    }
+    return;
+  }
+
+  //now push the open and do backtracking
+  curr.push("("); //Do
+  solve(curr,n,result); //call for other iteration
+  curr.pop(); //Undo
+
+  //now for close 
+  curr.push(")");
+  solve(curr,n,result);
+  curr.pop();
+}
+function isValid(curr){
+  let countOpen = 0;
+  for(let ch of curr){
+    if(ch === "("){
+      countOpen++;
+    }else{
+      countOpen--;
+      if(countOpen < 0){
+        return false;
+      }
+    }
+  }
+  return countOpen === 0;
+}
