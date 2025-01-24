2c76f7dbc4c23ef0d4fc8d5b2b2072c516116b38
Udai Chauhan <udaichauhan284@gmail.com>
2025-01-24 21:14:22 +0530

    ADD LCPOTD 802 in A2ZDSA Graph-TopoSort section

--- A2Z DSA Striver/Graphs/TopoSort and Problems/802FindEventualSafeStates.js
+++ A2Z DSA Striver/Graphs/TopoSort and Problems/802FindEventualSafeStates.js
@@ -42,6 +42,49 @@ function isCycleDFS(graph, curr, visited, inRecursion) {
   return false;
 }
 
+/*24 Jan 25,Method1 use of DFS, we have Directed graph and
+in this we need to find the safe nodes, so basically in 
+this we need to remove the nodes which are in cycle, so 
+detec cycle DFS will work here, use of visited and inRecur
+arr and at last those are not inRecurrsion, measn false
+we will return it.
+TC: O(V+E), SC: O(V)
+*/
+var eventualSafeNodes1 = function(graph) {
+  let v = graph.length; //n nodes
+  let safeNodes = []; //return ans
+  let visited = Array(v).fill(false);
+  let inRecursion = Array(v).fill(false);
+
+  //now call the cycleDFS for not visited
+  for(let i=0; i<v; i++){
+      if(!visited[i]){
+          isCycleDFS(graph, i, visited, inRecursion);
+      }
+  }
+
+  //now fill the safeNode
+  for(let i=0; i<v; i++){
+      if(!inRecursion[i]){
+          safeNodes.push(i);
+      }
+  }
+  return safeNodes;
+};
+function isCycleDFS(graph, curr, visited, inRecursion){
+  visited[curr] = true;
+  inRecursion[curr] = true;
+  for(let neighbor of graph[curr]){
+      if(!visited[neighbor] && isCycleDFS(graph, neighbor, visited, inRecursion)){
+          return true;
+      }else if(inRecursion[neighbor] === true){
+          return true;
+      }
+  }
+  inRecursion[curr] = false;
+  return false;
+}
+
 //Method2- solve b BFS, measn we need to implement Topo Sort by BFS, (Kahn's Algo), need to maintain the indegree and safe array, mark true,which came out from the queue. TC: TC: O(v+e), SC: O(v)
 var eventualSafeNodes1 = function(graph) {
   let v = graph.length;
@@ -86,5 +129,59 @@ var eventualSafeNodes1 = function(graph) {
       }
   }
   return safeNode;
+};
+
+
+
+/*24 Jan 25, Method2, use of BSF, in this we need to detect the cycle, so for this
+we can use the Topological Sort (Kahn's Algo), in this we reverse the edges to see the
+indegree and while traversing over node, decrease the indegree, when indegree is zero
+we add in queue, for futher. 
+TC: O(V+E), SC: O(V+E) for adj graph
+*/
+var eventualSafeNodes = function(graph) {
+  let v = graph.length;
+  let safeNodes = []; //return ans
+  let safe = Array(v).fill(false);
+  let indegree = Array(v).fill(0);
+  let queue = []; //for having the node of indegree 0 and for BFS
+  let adj = Array.from({length: v}, () => []);
+
+  //now fill the adj
+  for(let u=0; u<v; u++){
+      for(let neighbor of graph[u]){
+          adj[neighbor].push(u);
+          indegree[u]++;
+      }
+  }
+
+  //push the indegree 0 nodes in queue
+  for(let i=0; i<v; i++){
+      if(indegree[i] === 0){
+          queue.push(i);
+      }
+  }
+
+  //now apply BFS
+  let front = 0;
+  while(front < queue.length){
+      let curr = queue[front];
+      front++;
+      safe[curr] = true;
+      for(let v of adj[curr]){
+          indegree[v]--;
+          if(indegree[v] === 0){
+              queue.push(v);
+          }
+      }
+  }
+
+  //now add in safeNodes, those who are safe
+  for(let i=0; i<v; i++){
+      if(safe[i]){
+          safeNodes.push(i);
+      }
+  }
+  return safeNodes;
 };
 
