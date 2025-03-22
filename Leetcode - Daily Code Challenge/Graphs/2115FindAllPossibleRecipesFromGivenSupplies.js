720d0b0f43c47be14cc14f1e0c0eaf34b1556aee
Udai Chauhan <udaichauhan284@gmail.com>
2025-03-22 15:44:36 +0530

    Update 2115 in findAllRecipes question in Graph Section

--- Leetcode - Daily Code Challenge/Graphs/2115FindAllPossibleRecipesFromGivenSupplies.js
+++ Leetcode - Daily Code Challenge/Graphs/2115FindAllPossibleRecipesFromGivenSupplies.js
@@ -37,3 +37,53 @@ function findAllRecipes(recipes, ingredients, supplies) {
     }
     return result;
 }
+
+
+/*Use of Graph
+//T.C : (n + m + S)
+//S.C : O(n+S)
+*/
+var findAllRecipes = function (recipes, ingredients, supplies) {
+    let n = recipes.length;
+    let supplySet = new Set(supplies);
+    
+    let adj = new Map(); // ingredient -> list of recipe indices
+    let indegree = new Array(n).fill(0);
+    
+    for (let i = 0; i < n; i++) {
+        for (let ing of ingredients[i]) {
+            if (!supplySet.has(ing)) {
+                if (!adj.has(ing)) {
+                    adj.set(ing, []);
+                }
+                adj.get(ing).push(i);
+                indegree[i]++;
+            }
+        }
+    }
+    
+    let queue = [];
+    for (let i = 0; i < n; i++) {
+        if (indegree[i] === 0) {
+            queue.push(i);
+        }
+    }
+    
+    let result = [];
+    while (queue.length > 0) {
+        let i = queue.shift();
+        let recipe = recipes[i];
+        result.push(recipe);
+        
+        if (adj.has(recipe)) {
+            for (let idx of adj.get(recipe)) {
+                indegree[idx]--;
+                if (indegree[idx] === 0) {
+                    queue.push(idx);
+                }
+            }
+        }
+    }
+    
+    return result;
+};
