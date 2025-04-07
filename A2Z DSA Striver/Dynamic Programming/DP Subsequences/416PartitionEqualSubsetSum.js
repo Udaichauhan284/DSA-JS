c1e1901dbb24105c3fb5d078b6e2e684dd440c3e
Udai Chauhan <udaichauhan284@gmail.com>
2025-04-07 08:25:37 +0530

    Update in 416 LC POTD

--- A2Z DSA Striver/Dynamic Programming/DP Subsequences/416PartitionEqualSubsetSum.js
+++ A2Z DSA Striver/Dynamic Programming/DP Subsequences/416PartitionEqualSubsetSum.js
@@ -9,37 +9,37 @@ if s is odd, we cant divide in two equal part, so return false
 here we can use subset sum equal to k, and k will be sum/2
 TC: O(n*target), SC: O(n), we will use space optimization one
 */
-var canPartition = function (nums) {
+var canPartition = function(nums) {
   let n = nums.length;
   let sum = 0;
-  for (let i = 0; i < n; i++) {
-    sum += nums[i];
+  for(let i=0; i<n; i++){
+      sum += nums[i];
   }
-  if (sum % 2 === 1) return false; //odd sum, cant divide into two/
-  let target = Math.floor(sum / 2);
-  return subsetSumEqualToK(nums, n, target);
+  if(sum % 2 === 1) return false; //odd sum, cant divide into two/
+  let target = Math.floor(sum/2);
+  return subsetSumEqualToK(nums,n,target);
 };
-function subsetSumEqualToK(nums, n, target) {
-  let prev = Array(target + 1).fill(false);
+function subsetSumEqualToK(nums,n,target){
+  let prev = Array(target+1).fill(false);
   //base case
   prev[0] = true;
-  if (nums[0] <= target) {
-    prev[nums[0]] = true;
+  if(nums[0] <= target){
+      prev[nums[0]] = true;
   }
   //movement start
-  for (let i = 1; i < n; i++) {
-    let curr = Array(target + 1).fill(false);
-    for (let k = 1; k <= target; k++) {
-      let notTake = prev[k];
-      let take = false;
-      if (nums[i] <= k) {
-        take = prev[k - nums[i]];
-      } else {
-        tale = false;
+  for(let i=1; i<n; i++){
+      let curr = Array(target+1).fill(false);
+      for(let k=1; k<=target; k++){
+          let notTake = prev[k];
+          let take = false;
+          if(nums[i] <= k){
+              take = prev[k-nums[i]];
+          }else{
+              tale = false;
+          }
+          curr[k] = (notTake || take);
       }
-      curr[k] = notTake || take;
-    }
-    prev = curr;
+      prev = curr;
   }
   return prev[target];
 }
