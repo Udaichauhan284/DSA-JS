492c1bf10cfe51e84ed92616905c519e995a89f5
Udai Chauhan <udaichauhan284@gmail.com>
2025-04-19 18:45:05 +0530

    UPDATE 2563 LC POTD in Binary Search Section

--- Leetcode - Daily Code Challenge/BInary Search/2563CountTheNumberofFairPairs.js
+++ Leetcode - Daily Code Challenge/BInary Search/2563CountTheNumberofFairPairs.js
@@ -6,65 +6,50 @@ Output: 6
 Explanation: There are 6 fair pairs: (0,3), (0,4), (0,5), (1,3), (1,4), and (1,5).
 */
 
-/*In this we need to find the sum which is less then lower and
-sums which are lower then upper, x will be sum of pairs which are
-lower then lower value and y will be sum of pairs which are less
-then upper value. we will find the count += y-x;
-now we will find the lower_bound with lower-nums[i] and then 
-will be find upper_bound with upper-nums[i]
-lower bound will return first smallest index which is greater amd 
-quqal to target, but we need i+1, to target, so we will do 
-x = idx - 1 - i, same goes in upper bound. 
-TC: for sorting O(nlogn) and then in loop O(n logn ) ~ O(nlogn)
-SC: O(1)
+/* 19Apr25, LeetcodePOTD
+in this we need to use the lower and upper bound, low <= pairSum 
+<= upper, xPairSum < lower and yPairSum <= upper
+y-x will give the result, and for finding the nums, we can use the
+lower and upper bound
+TC: O(nlogn), SC: O(1)
 */
+var countFairPairs = function(nums, lower, upper) {
+    let len = nums.length;
+    nums.sort((a,b) => a-b);
 
-const countFairPairs = function(nums, lower, upper) {
-  let len = nums.length;
-  //first we need to sort the nums
-  nums.sort((a,b) => a-b); //now we can perform lower and upper
-  //bound on it.
-
-  //now main logic
-  let result = 0;
-  for(let i=0; i<len; i++){
-      let lowerIdx = lowerBound(i+1, len-1, lower-nums[i], nums); //low, high, target, arr
-      let x = lowerIdx - 1 - i;
-
-      let upperIdx = upperBound(i+1, len-1, upper-nums[i], nums); //low,high,target, arr
-      let y = upperIdx - 1 - i;
-
-      //now getting the count of pairSum which is in range of
-      // lower <= pairSum <= upper , we will do
-      result += (y - x);
-  }
-  return result;
+    let result = 0;
+    for(let i=0; i<len; i++){
+        let lowerIdx = lowerBound(i+1,len-1,lower-nums[i],nums);
+        let x = lowerIdx - 1 - i;
+        let upperIdx = upperBound(i+1,len-1,upper-nums[i],nums);
+        let y = upperIdx - 1 - i;
+        result += (y-x);
+    }
+    return result;
 };
 function lowerBound(low, high, target, nums){
-  let ans = high+1;
-  while(low <= high){
-      let mid = low + Math.floor((high-low)/2);
-      if(nums[mid] >= target){
-          //this will return the first smallest index which is just greater
-          //and equal then target
-          ans = mid;
-          high = mid - 1;
-      }else{
-          low = mid+1;
-      }
-  }
-  return ans;
+    let ans = high+1;
+    while(low <= high){
+        let mid = low + Math.floor((high-low)/2);
+        if(nums[mid] >= target){
+            ans = mid;
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return ans;
 }
 function upperBound(low, high, target, nums){
-  let ans = high+1;
-  while(low <= high){
-      let mid = low + Math.floor((high - low)/2);
-      if(nums[mid] > target){
-          ans = mid;
-          high = mid-1;
-      }else{
-          low = mid + 1;
-      }
-  }
-  return ans;
+    let ans = high+1;
+    while(low <= high){
+        let mid = low + Math.floor((high-low)/2);
+        if(nums[mid] > target){
+            ans = mid;
+            high = mid - 1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return ans;
 }
