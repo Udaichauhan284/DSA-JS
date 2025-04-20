59f2b62bf3f1673c826d7baf95585c053941837f
Udai Chauhan <udaichauhan284@gmail.com>
2025-04-21 01:28:35 +0530

    UPDATE THE LC POTD 781 in HashMap Section

--- Leetcode - Daily Code Challenge/Hash Maps/781RabbitsInForest.js
+++ Leetcode - Daily Code Challenge/Hash Maps/781RabbitsInForest.js
@@ -18,3 +18,29 @@ var numRabbits = function(answers) {
 
     return count;
 };
+
+
+/*In this we will have x which is answer given by rabit,
+groupSize will be x+1, means in this group, we have x+1 rabbits
+now find the groups in which multple rabbits will there
+groups = count / groupSize, count is x ans given by multiple rabit
+rabbits find groups * groupSize
+TC: O(n), SC: O(n)
+*/
+var numRabbits = (answers) => {
+    let freq = new Map();
+    for(let ans of answers){
+        freq.set(ans, (freq.get(ans) || 0)+1);
+    }
+
+    let totalRabbit = 0;
+    //iterte over the freq map
+    //x is answer given, count will how many times x is there
+    for(let [x, count] of freq){
+        let groupSize = x+1;
+        let group = Math.ceil(count / groupSize);
+
+        totalRabbit += group * groupSize;
+    }
+    return totalRabbit;
+}
