d978bae1d5726f1776a080f9bd5cd1124a401414
Udai Chauhan <udaichauhan284@gmail.com>
2025-04-26 22:39:04 +0530

    UPDATE 2845 in Array Medium Section with detail code

--- Leetcode - Daily Code Challenge/Arrays/Medium/2845CountOfInterstingSubarrays.js
+++ Leetcode - Daily Code Challenge/Arrays/Medium/2845CountOfInterstingSubarrays.js
@@ -28,3 +28,44 @@ var countInterestingSubarrays = function(nums, modulo, k) {
     }
     return res;
 };
+
+
+
+/*In this we can use the LC 974 logic, subarray sum 
+equal to k, sum1 is something, s2, is something
+we can find the (sum1-sum2)%mod === k, in this we can add
+mod (sum1%mod - sum2%mod + mod)%mod === k
+sum1%mod = r1, sum2mod =r2
+(r1-r2)%mod === k => (r1-k+mod)%mod === r2
+TC: O(n), SC: O(n)
+*/
+var countInterestingSubarrays = function(nums, modulo, k) {
+    let len = nums.length;
+    for(let i=0; i<len; i++){
+        if(nums[i] % modulo === k){
+            nums[i] = 1;
+        }else{
+            nums[i] = 0;
+        }
+    }
+    //now using this, it will easy to find the sum
+    //subarray
+    let count = 0;
+    let sum = 0;
+    let map = new Map(); //remainder -> count
+    //we alredy seen 0 rem
+    map.set(0,1);
+    for(let i=0; i<len; i++){
+        sum += nums[i];
+
+        let r1 = sum % modulo;
+        let r2 = (r1-k+modulo)%modulo;
+
+        //increase the count if r2 seen in map
+        if(map.has(r2)){
+            count += map.get(r2) || 0;
+        }
+        map.set(r1, (map.get(r1) || 0)+1);
+    }
+    return count;
+};
