518d9038473eae393702fbc0fa157bdbd05e920b
Udai Chauhan <udaichauhan284@gmail.com>
2025-04-27 12:33:25 +0530

    Trying commit in past date in Array Seasy section 3392

--- Leetcode - Daily Code Challenge/Arrays/Easy/3392CountSubarraysofLengthThreeWithACondition.js
+++ Leetcode - Daily Code Challenge/Arrays/Easy/3392CountSubarraysofLengthThreeWithACondition.js
@@ -9,6 +9,7 @@ Explanation:
 Only the subarray [1,4,1] contains exactly 3 elements where the sum of the first and third numbers equals half the middle number.
 */
 
+//Easy Mark Question
 /*In this we take 3 elem from nums i, i+1, i+2, and we go till
 the len-2, and check the condition
 TC: O(n), SC: O(1)
