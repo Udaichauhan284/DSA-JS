1498beeb9bf3aab76ccd695e285936d2c4b575ff
Udai Chauhan <udaichauhan284@gmail.com>
2025-05-11 21:31:46 +0530

    UPDATE LC POTD 1550 in Array Easy section of A2Z DSAA

--- A2Z DSA Striver/Array/Easy/1550ThreeConsecutiveOdds.js
+++ A2Z DSA Striver/Array/Easy/1550ThreeConsecutiveOdds.js
@@ -21,3 +21,13 @@ var threeConsecutiveOdds = function (arr) {
   }
   return false;
 };
+
+var threeConsecutiveOdds = function(arr) {
+  let len = arr.length;
+  for(let i=0; i<len-2; i++){
+      if(arr[i] % 2 !== 0 && arr[i+1] % 2 !== 0 && arr[i+2] % 2 !== 0){
+          return true;
+      }
+  }
+  return false;
+};
