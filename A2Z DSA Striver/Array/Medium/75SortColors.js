b2ecc43478d1ff239a217ecd1c6e1d3ca8e1b56f
Udai Chauhan <udaichauhan284@gmail.com>
2025-05-17 23:18:22 +0530

    Update the sort colors 75 in A2ZDSA Array Medium Section

--- A2Z DSA Striver/Array/Medium/75SortColors.js
+++ A2Z DSA Striver/Array/Medium/75SortColors.js
@@ -37,30 +37,61 @@ TC: O(2n), SC: O(1)
 //     return nums;
 // }
 
-/* Method3 - use of Dutch Flag Method, take 3 pointer low,mid = 0
-and high = nums.length-1;
-we move mid till mid <= high, and see if nums[mid] = 0, we swap 
-with mid and low-move pointer low++, mid++, if mid ===1, move mid
-only, if mid === 2, swap mid and high
-TC: O(n), SC: O(1)
+//Method-1, TC: O(nlogn), SC: O(1)
+// var sortColors = function(nums) {
+//     return nums.sort((a,b) => a-b);
+// };
+
+//Method-2, we use the countZero and countOne and start the loop
+//till countZero put the zero then from zero to zero+one, then till
+//n. TC: O(2n), SC: O(1)
+// var sortColors = function(nums) {
+//     let len = nums.length;
+//     let countZero = 0;
+//     let countOne = 0;
+//     for(let i=0; i<len; i++){
+//         if(nums[i] === 0) countZero++;
+//         if(nums[i] === 1) countOne++;
+//     }
+
+//     //from zero to countZero
+//     for(let i=0; i<countZero; i++){
+//         nums[i] = 0;
+//     }
+//     //from countZero to countZero+countOne
+//     for(let i=countZero; i<countZero+countOne; i++){
+//         nums[i] = 1;
+//     }
+//     for(let i=countZero+countOne; i<len; i++){
+//         nums[i] = 2;
+//     }
+//     return nums;
+// };
+
+/*Method 3, in one pass, we can use the low denote the zero,
+mid denotes the one and high denotes the high, we iterate over the
+mid to high, and if we see mid === 1, increase the mid, mid === 0
+0 needs to go the first place, swap the low and mid, and increase
+the mid and low, and if mid === 2, swap the mid and high and 
+descrease the high. TC: O(n), SC: O(1)
 */
-const sortColors = (nums) => {
-  let low = 0, mid = 0, high = nums.length-1;
-  while(mid <= high){
-      if(nums[mid] === 1){
-          mid++;
-      }
-      else if(nums[mid] === 0){
-          //swap low and mid
-          [nums[low], nums[mid]] = [nums[mid], nums[low]];
-          low++;
-          mid++;
-      }else{
-          //measn nums[mid] === 2
-          //swap mid and high
-          [nums[mid], nums[high]] = [nums[high], nums[mid]];
-          high--;
-      }
-  }
-  return nums;
+var sortColors = (nums) => {
+    let low = 0, mid = 0, high = nums.length-1;
+    while(mid <= high){
+        if(nums[mid] === 1){
+            //we want one in middle
+            mid++;
+        }
+        else if(nums[mid] === 0){
+            //we want zero in starting
+            [nums[mid], nums[low]] = [nums[low], nums[mid]];
+            low++;
+            mid++;
+        }else{
+            //mid is 2
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+        }
+    }
+    return nums;
 }
