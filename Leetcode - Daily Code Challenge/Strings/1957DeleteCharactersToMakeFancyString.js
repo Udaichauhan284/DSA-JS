31ab809ea6c8f989cb2138882fd06a4c163697a2
Udai Chauhan <udaichauhan284@gmail.com>
2025-07-21 23:44:50 +0530

    Update the 1957 LC POTD

--- Leetcode - Daily Code Challenge/Strings/1957DeleteCharactersToMakeFancyString.js
+++ Leetcode - Daily Code Challenge/Strings/1957DeleteCharactersToMakeFancyString.js
@@ -32,3 +32,38 @@ var makeFancyString = function (s) {
     }
     return result.join('');
 };
+
+
+
+
+/*21 July 2025, Leetcode POTD
+in this first we push the first char in
+result array, and then in loop we check
+new char is same as result previous one
+then we increase the freq and if freq is
+less then 3 we push into result,
+else if next char is not equal to result
+prev means that is new char, we push into
+result and make freq = 1;
+TC: O(n), SC: O(1)
+*/
+var makeFancyString = function(s) {
+    let len = s.length;
+    let result = [];
+    result.push(s[0]);
+    let freq = 1;
+    for(let i=1; i<len; i++){
+        if(s[i] === result[result.length-1]){
+            //checking the curr char with result prev one
+            freq++; //increase the freq 
+            if(freq < 3){
+                //now check the freq is less 3, push in result
+                result.push(s[i]);
+            }
+        }else{
+            result.push(s[i]);
+            freq = 1;
+        }
+    }
+    return result.join('');
+};
