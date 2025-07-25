261080a6eebceb232aa2959451610484bfb17364
Udai Chauhan <udaichauhan284@gmail.com>
2025-07-25 13:05:05 +0530

    UPDATE 3487 in array easy with method 2

--- Leetcode - Daily Code Challenge/Arrays/Easy/3487MaximumUniqueSubarraySumAfterDeletion.js
+++ Leetcode - Daily Code Challenge/Arrays/Easy/3487MaximumUniqueSubarraySumAfterDeletion.js
@@ -32,3 +32,33 @@ var maxSum = function(nums) {
     }
     return sum === 0  ? maxNeg : sum;
 };
+
+
+
+/*In this we need a sum of positive nums
+as we need maxSubarray, so we can ignore 
+the -ve nums, we can take that in other
+variable negNum, else if we maintain the
+set, and check is that currnum is in set
+we skip if not, we add in max
+TC: O(n), SC: O(n)for set
+
+//In Method 2, in place of set, we can also maintain
+an array of 101, in constraints, nums[i] going till 100
+TC: O(n), SC: O(101) ~ O(1)
+*/
+var maxSum = function(nums) {
+    let unique = Array(101).fill(-1);
+    let sum = 0;
+    let maxNeg = -Infinity;
+    for(let num of nums){
+        if(num <= 0){
+            maxNeg = Math.max(maxNeg, num);
+        }else if(unique[num] === -1){
+            //if arr has not that num, add in sum
+            sum += num;
+            unique[num] = 1;
+        }
+    }
+    return sum === 0  ? maxNeg : sum;
+};
