21faedf41441e5e7abd2a6d49abd066d7122c0ad
Udai Chauhan <udaichauhan284@gmail.com>
2025-08-03 17:36:53 +0530

    UPDATE The 898 with new learning and code

--- Leetcode - Daily Code Challenge/Hash Maps/898BitwiseORsOfSubarrays.js
+++ Leetcode - Daily Code Challenge/Hash Maps/898BitwiseORsOfSubarrays.js
@@ -25,3 +25,56 @@ var subarrayBitwiseORs = function(arr) {
 
     return result.size;
 };
+
+
+/*In Brute Method, we iterate over the arr
+and start finding the OR for subarrays
+and add in set, so at last we will get the
+uniqueOR length. TC: O(n^2), SC: O(n)
+this will give the TLE, 
+
+in method 2
+so we need to maintain
+the prev values of OR also so we dont need
+to iterate nested.
+*/
+var subarrayBitwiseORs = function(arr) {
+    let len = arr.length;
+    let uniqueOR = new Set();
+    for(let i=0; i<len; i++){
+        let currOR = arr[i];
+        for(let j=i; j<len; j++){
+            currOR |= arr[j];
+            uniqueOR.add(currOR);
+        }
+    }
+    return uniqueOR.size;
+};
+
+
+/*In method2, we will maintain the prev set
+to store the prev OR, so while calculate OR for
+currOne, we will use the prev OR vals
+TC: O(n*32)~O(n), in set we will maintain the 32 bits
+SC: O(n*32)~O(n)
+*/
+var subarrayBitwiseORs = function(arr) {
+    let len = arr.length;
+    let prev = new Set();
+    let uniqueOR = new Set();
+
+    for(let i=0; i<len; i++){
+        let currOne = new Set();
+        for(let x of prev){
+            currOne.add(x | arr[i]);
+            uniqueOR.add(x | arr[i]);
+        }
+        //now add the single elem, which is also
+        //subarrays
+        currOne.add(arr[i]);
+        uniqueOR.add(arr[i]);
+        //now change the prev to curr
+        prev = currOne;
+    }
+    return uniqueOR.size;
+};
