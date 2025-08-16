4b81f8288e120865c91dc673b8984eef38e5e36e
Udai Chauhan <udaichauhan284@gmail.com>
2025-08-16 23:27:27 +0530

    revision of 2787 lc ques in dp section

--- Leetcode - Daily Code Challenge/DP/2787WasyToExpressAnIntegerasSumOfPowers.js
+++ Leetcode - Daily Code Challenge/DP/2787WasyToExpressAnIntegerasSumOfPowers.js
@@ -32,3 +32,66 @@ var numberOfWays = function(n, x) {
     t = Array.from({ length: n + 1 }, () => Array(301).fill(-1));
     return solve(n, 1, x);
 };
+
+
+
+/*In this we can use the recursion, we can simple
+take num start from 1 and check 1^x is less then
+n, means we can take that num or skip it, if we
+take it, minus from n or skip it
+TC: O(2^n), for recursion
+SC: O(n) for recursion stack space 
+TLE
+*/
+var numberOfWays = function(n, x) {
+    return solve(n,1,x); //n,num,x
+};
+const solve = (n,num,x) => {
+    //base case
+    if(n === 0) return 1;
+    if(n < 0) return 0;
+    let powerOfNum = Math.pow(num,x);
+    if(powerOfNum > n) return 0;
+    //now use the recursion
+    let take = solve(n-powerOfNum, num+1, x);
+    let skip = solve(n,num+1,x);
+    return take+skip;
+}
+
+
+/* Method 2, for removing the TLE, we can Memoize it.
+In this we can use the recursion, we can simple
+take num start from 1 and check 1^x is less then
+n, means we can take that num or skip it, if we
+take it, minus from n or skip it
+TC: O(2^n), for recursion
+SC: O(n) for recursion stack space 
+TLE
+
+TC: (n*n), SC: O(n*n)
+*/
+const M = 1e9 + 7;
+var numberOfWays = function(n, x) {
+    let dp = Array.from({length: n+1}, () => Array(n+1).fill(-1));
+    return solve(n,1,x,dp); //n,num,x
+};
+const solve = (n,num,x,dp) => {
+    //base case
+    if(n === 0) return 1;
+
+    if(n < 0) return 0;
+
+    if(num > n) return 0;
+
+    let powerOfNum = Math.pow(num,x);
+
+    //now check the dp array
+    if(dp[n][num] !== -1) return dp[n][num];
+
+    if(powerOfNum > n) return 0;
+    //now use the recursion
+    let take = solve(n-powerOfNum, num+1, x,dp);
+    let skip = solve(n,num+1,x,dp);
+    dp[n][num] = (take+skip)%M;
+    return dp[n][num];
+}
