44de5ad0789f73341c1f02d5fb6b12339d1c10f2
Udai Chauhan <udaichauhan284@gmail.com>
2025-09-12 00:21:26 +0530

    update the 2785 with method 2 in string section

--- Leetcode - Daily Code Challenge/Strings/2785SortVowelsInAString.js
+++ Leetcode - Daily Code Challenge/Strings/2785SortVowelsInAString.js
@@ -33,3 +33,41 @@ var sortVowels = function(s) {
 
     return result;
 };
+
+/*Method2, we use the counting sort, we will maintain the
+count map of vowel and in a string we have sorted vowel
+string "AEIOUaeiou"
+TC: O(n+n) ~ O(2n) ~ O(n) , SC: O(n)
+*/
+var sortVowels = function(s) {
+    let map = new Map(); //maintain freq of vowels
+    for(let ch of s){ //O(n)
+        if(isVowel(ch)){
+            map.set(ch, (map.get(ch) || 0)+1);
+        }
+    }
+    //we know sorted string of vowel
+    let sortedVowel = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']; //SC: O(n)
+    let j = 0; //pointer for sortedVowel
+    let result = "";
+    for(let ch of s){ //O(n)
+        if(isVowel(ch)){
+            while(j < sortedVowel.length && (!map.has(sortedVowel[j])) || map.get(sortedVowel[j]) === 0){
+                j++; //i dont get that vowel in sorted vowel, 
+                //so move pointer
+            }
+            result += sortedVowel[j];             // place smallest available vowel
+            map.set(sortedVowel[j], map.get(sortedVowel[j]) - 1); // decrease count
+        }else{
+            result += ch;
+        }
+    }
+    return result;
+};
+function isVowel(ch){
+    let newCh = ch.toLowerCase();
+    if(newCh === 'a' || newCh === 'e' || newCh === 'i' || newCh === 'o' || newCh === 'u'){
+        return true;
+    }
+    return false;
+}
