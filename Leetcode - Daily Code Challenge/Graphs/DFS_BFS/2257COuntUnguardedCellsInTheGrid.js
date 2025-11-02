43e7ebd1462b62cd957a1a0a688adabd6ca6bcc2
Udai Chauhan <udaichauhan284@gmail.com>
2025-11-02 23:37:29 +0530

    update the 2257

--- Leetcode - Daily Code Challenge/Graphs/DFS_BFS/2257COuntUnguardedCellsInTheGrid.js
+++ Leetcode - Daily Code Challenge/Graphs/DFS_BFS/2257COuntUnguardedCellsInTheGrid.js
@@ -11,47 +11,47 @@ There are a total of 7 unguarded cells, so we return 7.
 TC: O(n*m + G*(n*m))
 */
 var countUnguarded = function(m, n, guards, walls) {
-  let grid = Array.from({length: m}, () => Array(n).fill(0));
-  //fill for gaurd
-  for(let [i,j] of guards){
-      grid[i][j] = 1;
-  }
-  //fill the wall
-  for(let [i,j] of walls){
-      grid[i][j] = 2;
-  }
-  //now move into grid
-  for(let [row,col] of guards){
-      dfs(grid,row-1,col,m,n,1); //UP
-      dfs(grid,row+1,col,m,n,2); //DOWN
-      dfs(grid,row,col-1,m,n,3); //LEFT
-      dfs(grid,row,col+1,m,n,4); //RIGHT
-  }
-  let ungaurdedCell=0;
-  for(let i=0; i<grid.length; i++){
-      for(let j=0; j<grid[0].length; j++){
-          if(grid[i][j] === 0){
-              ungaurdedCell++;
-          }
-      }
-  }
-  return ungaurdedCell;
+    let grid = Array.from({length: m}, () => Array(n).fill(0));
+    //fill for gaurd
+    for(let [i,j] of guards){
+        grid[i][j] = 1;
+    }
+    //fill the wall
+    for(let [i,j] of walls){
+        grid[i][j] = 2;
+    }
+    //now move into grid
+    for(let [row,col] of guards){
+        dfs(grid,row-1,col,m,n,1); //UP
+        dfs(grid,row+1,col,m,n,2); //DOWN
+        dfs(grid,row,col-1,m,n,3); //LEFT
+        dfs(grid,row,col+1,m,n,4); //RIGHT
+    }
+    let ungaurdedCell=0;
+    for(let i=0; i<grid.length; i++){
+        for(let j=0; j<grid[0].length; j++){
+            if(grid[i][j] === 0){
+                ungaurdedCell++;
+            }
+        }
+    }
+    return ungaurdedCell;
 };
 function dfs(grid,row,col,m,n,direction){
-  //base case
-  if(row < 0 || col < 0 || row >= m || col >= n || grid[row][col] === 1 || grid[row][col] === 2){
-      return;
-  }
-  //mark visited
-  grid[row][col] = 3;
-  //move in direction
-  if(direction === 1){
-      dfs(grid,row-1,col,m,n, direction);
-  }else if(direction === 2){
-      dfs(grid,row+1,col,m,n,direction);
-  }else if(direction === 3){
-      dfs(grid,row,col-1,m,n,direction);
-  }else{
-      dfs(grid,row,col+1,m,n,direction);
-  }
+    //base case
+    if(row < 0 || col < 0 || row >= m || col >= n || grid[row][col] === 1 || grid[row][col] === 2){
+        return;
+    }
+    //mark visited
+    grid[row][col] = 3;
+    //move in direction
+    if(direction === 1){
+        dfs(grid,row-1,col,m,n, direction);
+    }else if(direction === 2){
+        dfs(grid,row+1,col,m,n,direction);
+    }else if(direction === 3){
+        dfs(grid,row,col-1,m,n,direction);
+    }else{
+        dfs(grid,row,col+1,m,n,direction);
+    }
 }
