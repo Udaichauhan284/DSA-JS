e6a391f0318d67f38a5d13412a2c0db2ee300cb6
Udai Chauhan <udaichauhan284@gmail.com>
2025-11-19 02:33:19 +0530

    update the 3408 with good and commented code

--- Leetcode - Daily Code Challenge/DS Design/3408DesignTaskManager.js
+++ Leetcode - Daily Code Challenge/DS Design/3408DesignTaskManager.js
@@ -22,10 +22,10 @@ class MyMaxHeap {   // <---- renamed
     constructor() {
         this.data = [];
     }
-   _compare(a, b) {
+    _compare(a, b) {
     if (a[0] === b[0]) return a[1] > b[1];
     return a[0] > b[0];
-}
+    }
 
     push(item) {
         this.data.push(item);
@@ -106,6 +106,123 @@ class TaskManager {
         while (this.maxHeap.size() > 0) {
             const [priority, taskId] = this.maxHeap.pop();
             if (priority === this.taskPriorityMap.get(taskId)) {
+                this.taskPriorityMap.set(taskId, -1);
+                return this.taskOwnerMap.get(taskId);
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+// ====> 19 Nov 2025 <====
+class MyMaxHeap{
+    constructor(){
+        this.data = [];
+    }
+    //compare function if both priority same, check on taskId
+    compare(a, b){
+        if(a[0] === b[0]) return a[1] > b[1];
+        return a[0] > b[0];
+    }
+    getParentIndex(idx){
+        return Math.floor((idx-1)/2);
+    }
+    getLeftChildIndex(idx){
+        return 2*idx+1
+    }
+    getRightChildIndex(idx){
+        return 2*idx+2;
+    }
+    swap(i1, i2){
+        [this.data[i1], this.data[i2]] = [this.data[i2], this.data[i1]];
+    }
+    push(pair){
+        this.data.push(pair);
+        this.heapifyUp();
+    }
+    heapifyUp(){
+        let idx = this.data.length-1;
+        while(idx > 0){
+            let parentIndex = this.getParentIndex(idx);
+            if(this.compare(this.data[idx], this.data[parentIndex])){
+                this.swap(idx, parentIndex);
+                idx = parentIndex;
+            }else{
+                break;
+            }
+        }
+    }
+    poll(){
+        if(this.data.length === 0) return null;
+        if(this.data.length === 1) return this.data.pop();
+        let maxValue = this.data[0];
+        this.data[0] = this.data.pop();
+        this.heapifyDown(0);
+        return maxValue;
+    }
+    heapifyDown(idx){
+        while(true){
+            let largest = idx;
+            let left = this.getLeftChildIndex(idx);
+            let right = this.getRightChildIndex(idx);
+            if(left < this.data.length && this.compare(this.data[left] , this.data[largest])){
+                largest = left;
+            }
+            if(right < this.data.length && this.compare(this.data[right] , this.data[largest])){
+                largest = right;
+            }
+            if(largest !== idx){
+                this.swap(idx, largest);
+                idx = largest;
+            }else{
+                break;
+            }
+        }
+    }
+    peek(){
+        return this.data[0];
+    }
+    isEmpty(){
+        return this.data.length === 0;
+    }
+}
+class TaskManager{
+    constructor(tasks){
+        this.maxHeap = new MyMaxHeap(); //[priority, taskId]
+        this.taskPriorityMap = new Map(); //[taskId, priority]
+        this.taskOwnerMap = new Map(); //[taskId, userId]
+
+        //first add initial task
+        for(let [userId, taskId, priority] of tasks){
+            this.add(userId, taskId, priority);
+        }
+    }
+
+    add(userId, taskId, priority){
+        //now add into the maxHeap
+        this.maxHeap.push([priority, taskId]); //O(logn)
+        this.taskPriorityMap.set(taskId, priority); //O(1) 
+        this.taskOwnerMap.set(taskId, userId); //O(1)
+    }
+    edit(taskId, newPriority){
+        //now add the newPriority into the mapHeap
+        this.maxHeap.push([newPriority, taskId]); //O(logn)
+        this.taskPriorityMap.set(taskId, newPriority); //O(1)
+    }
+    rmv(taskId){
+        //simply mark the taskId into the taskPriorityMap with -1, means stale it
+        this.taskPriorityMap.set(taskId, -1); //O(1)
+    }
+    execTop(){
+        //in this we neep to excute the top priority from maxHeap one
+        while(! this.maxHeap.isEmpty()){ //O(nlogn)
+            //now first take out the priority, taskId from maxheap
+            const [priority, taskId] = this.maxHeap.poll(); //O(logn)
+            if(priority === this.taskPriorityMap.get(taskId)){
+                //is inside if, means we have fresh one, make it stale, and return
+                //user
                 this.taskPriorityMap.set(taskId, -1);
                 return this.taskOwnerMap.get(taskId);
             }
