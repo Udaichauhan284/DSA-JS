fd8759fef2c3a2592bf8194a7b4bf028338b88c3
Udai Chauhan <udaichauhan284@gmail.com>
2025-11-21 12:51:54 +0530

    update the 1930

--- Leetcode - Daily Code Challenge/Strings/1930UniqueLength3PalindromicSubsequences.js
+++ Leetcode - Daily Code Challenge/Strings/1930UniqueLength3PalindromicSubsequences.js
@@ -19,36 +19,36 @@ range we find the unique letters, to put in new set, so the
 size of set is our ans, TC: O(26*2n)~O(n), SC: O(26)~O(1)
 */
 var countPalindromicSubsequence = function(s) {
-  let len = s.length;
-  let letters = new Set(); //O(26)
-  let result = 0;
-  //now fill the letters set with unique letters
-  for(let ch of s){ //O(n)
-      letters.add(ch);
-  }
-  //now we will travese over the letters set and give the chance
-  //to every unique letters to form the palindromic subsequence
-  for(let letter of letters){ //O(26)
-      let leftIdx = -1;
-      let rightIdx = -1;
-      for(let i=0; i<len; i++){  //O(n)
-          if(s[i] === letter){
-              if(leftIdx === -1){
-                  leftIdx = i;
-              }
-              rightIdx = i;
-          }
-      }
+    let len = s.length;
+    let letters = new Set(); //O(26)
+    let result = 0;
+    //now fill the letters set with unique letters
+    for(let ch of s){ //O(n)
+        letters.add(ch);
+    }
+    //now we will travese over the letters set and give the chance
+    //to every unique letters to form the palindromic subsequence
+    for(let letter of letters){ //O(26)
+        let leftIdx = -1;
+        let rightIdx = -1;
+        for(let i=0; i<len; i++){  //O(n)
+            if(s[i] === letter){
+                if(leftIdx === -1){
+                    leftIdx = i;
+                }
+                rightIdx = i;
+            }
+        }
 
-      //now traverse in between the left and right idx to 
-      //find the middle letters
-      let uniqueMiddle = new Set();
-      for(let middle=leftIdx+1; middle<=rightIdx-1; middle++){ //O(n)
-          uniqueMiddle.add(s[middle]);
-      }
-      result += uniqueMiddle.size;
-  }
-  return result;
+        //now traverse in between the left and right idx to 
+        //find the middle letters
+        let uniqueMiddle = new Set();
+        for(let middle=leftIdx+1; middle<=rightIdx-1; middle++){ //O(n)
+            uniqueMiddle.add(s[middle]);
+        }
+        result += uniqueMiddle.size;
+    }
+    return result;
 };
 
 
@@ -58,35 +58,35 @@ and then travere over the 26 len and find out the left and right
 and then middle, TC: O(n), SC:O(1)
 */
 var countPalindromicSubsequence = function(s) {
-  let len = s.length;
-  let indices = Array.from({length: 26}, ()=>Array(26).fill(-1));
-  //now traverse over the s and find left and right for all
-  //char
-  for(let i=0; i<len; i++){
-      let idx = s.charCodeAt(i)-'a'.charCodeAt(0); //a->0,b->1..
-      if(indices[idx][0] === -1){
-          indices[idx][0] = i;
-      }
-      indices[idx][1] = i; //rightIdx
-  }
+    let len = s.length;
+    let indices = Array.from({length: 26}, ()=>Array(26).fill(-1));
+    //now traverse over the s and find left and right for all
+    //char
+    for(let i=0; i<len; i++){
+        let idx = s.charCodeAt(i)-'a'.charCodeAt(0); //a->0,b->1..
+        if(indices[idx][0] === -1){
+            indices[idx][0] = i;
+        }
+        indices[idx][1] = i; //rightIdx
+    }
 
-  //now traverse over the 26 letters
-  let result = 0;
-  for(let i=0; i<26; i++){
-      let leftIdx = indices[i][0];
-      let rightIdx = indices[i][1];
+    //now traverse over the 26 letters
+    let result = 0;
+    for(let i=0; i<26; i++){
+        let leftIdx = indices[i][0];
+        let rightIdx = indices[i][1];
 
-      //if we dont have left and right, skip
-      if(leftIdx === -1 && rightIdx === -1){
-          continue;
-      }
+        //if we dont have left and right, skip
+        if(leftIdx === -1 && rightIdx === -1){
+            continue;
+        }
 
-      //now trvaerse over the range
-      let uniqueMiddle = new Set();
-      for(let middle=leftIdx+1; middle<=rightIdx-1; middle++){
-          uniqueMiddle.add(s[middle]);
-      }
-      result += uniqueMiddle.size;
-  }
-  return result;
+        //now trvaerse over the range
+        let uniqueMiddle = new Set();
+        for(let middle=leftIdx+1; middle<=rightIdx-1; middle++){
+            uniqueMiddle.add(s[middle]);
+        }
+        result += uniqueMiddle.size;
+    }
+    return result;
 };
