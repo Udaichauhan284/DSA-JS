b6aae483a225968ea744ee207ce651a857ab9e41
Udai Chauhan <udaichauhan284@gmail.com>
2025-12-12 23:41:48 +0530

    update the 2435

--- Leetcode - Daily Code Challenge/DP/DP On Grid/2435PathsInMatrixWhoseSumIsDivisibleByK.js
+++ Leetcode - Daily Code Challenge/DP/DP On Grid/2435PathsInMatrixWhoseSumIsDivisibleByK.js
@@ -36,3 +36,50 @@ function numberOfPaths(grid, k) {
 
     return solve(0, 0, 0);
 }
+
+
+
+//12 Dec 2025, leetcode, HARD
+/* Method 1: Recursion + Memo
+   TC: O(n*m*k), SC: O(n*m*k)
+*/
+const M = 1e9 + 7;
+
+var numberOfPaths = function (grid, k) {
+    let n = grid.length;
+    let m = grid[0].length;
+
+    // dp[n][m][k]
+    let dp = Array.from({ length: n }, () =>
+        Array.from({ length: m }, () =>
+            Array(k).fill(-1)
+        )
+    );
+
+    return solve(0, 0, 0, grid, k, n, m, dp);
+};
+
+function solve(i, j, currSum, grid, k, n, m, dp) {
+
+    // boundary check (must be >=)
+    if (i >= n || j >= m) return 0;
+
+    // if last cell reached
+    if (i === n - 1 && j === m - 1) {
+        return ((currSum + grid[i][j]) % k === 0 ? 1 : 0);
+    }
+
+    // memo check
+    if (dp[i][j][currSum] !== -1) return dp[i][j][currSum];
+
+    // explore both directions with updated sum
+    let newSum = (currSum + grid[i][j]) % k;
+
+    let down = solve(i + 1, j, newSum, grid, k, n, m, dp);
+    let right = solve(i, j + 1, newSum, grid, k, n, m, dp);
+
+    // store result
+    dp[i][j][currSum] = (down + right) % M;
+
+    return dp[i][j][currSum];
+}
