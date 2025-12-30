af209419f0a270e824f9bddbd57d64cce8a40742
Udai Chauhan <udaichauhan284@gmail.com>
2025-12-30 23:08:49 +0530

    update 840

--- Leetcode - Daily Code Challenge/Arrays/Medium/840MagicSquaresInGrid.js
+++ Leetcode - Daily Code Challenge/Arrays/Medium/840MagicSquaresInGrid.js
@@ -17,51 +17,51 @@ TC: O(n*m) for row-3 and col-3, O(1) for magic square
 SC: O(3*3) set ~ O(1)
 */
 var numMagicSquaresInside = function(grid) {
-  let rows = grid.length;
-  let cols = grid[0].length;
-  let count = 0;
-  for(let i=0; i<=(rows-3); i++){
-      for(let j=0; j<=(cols-3); j++){
-          if(isMagicSquare(grid,i,j)){
-              count++;
-          }
-      }
-  }
-  return count;
-};
-function isMagicSquare(grid, r, c){
-  let set = new Set(); //SC: O(3*3)~ O(1), for distinct 
-  for(let i=0; i<3; i++){
-      for(let j=0; j<3; j++){
-          let num = grid[r+i][c+j]; //0+0=0, 1+0=1
-          if(num < 1 || num > 9 || set.has(num)){
-              return false; // not distinct and not in 1-9
-          }else{
-              set.add(num);
-          }
-      }
-  }
+    let rows = grid.length;
+    let cols = grid[0].length;
+    let count = 0;
+    for(let i=0; i<=(rows-3); i++){
+        for(let j=0; j<=(cols-3); j++){
+            if(isMagicSquare(grid,i,j)){
+                count++;
+            }
+        }
+    }
+    return count;
+    };
+    function isMagicSquare(grid, r, c){
+    let set = new Set(); //SC: O(3*3)~ O(1), for distinct 
+    for(let i=0; i<3; i++){
+        for(let j=0; j<3; j++){
+            let num = grid[r+i][c+j]; //0+0=0, 1+0=1
+            if(num < 1 || num > 9 || set.has(num)){
+                return false; // not distinct and not in 1-9
+            }else{
+                set.add(num);
+            }
+        }
+    }
 
-  //row sum
-  let rSum = grid[r][c]+grid[r][c+1]+grid[r][c+2];
-  //lets find out row sun and col sum
-  for(let i=0; i<3; i++){
-      if(grid[r+i][c]+grid[r+i][c+1]+grid[r+i][c+2] !== rSum){
-          return false;
-      }
-      //now move col
-      if(grid[r][c+i]+grid[r+1][c+i]+grid[r+2][c+i] !== rSum){
-          return false;
-      }
-  }
+    //row sum
+    let rSum = grid[r][c]+grid[r][c+1]+grid[r][c+2];
+    //lets find out row sun and col sum
+    for(let i=0; i<3; i++){
+        if(grid[r+i][c]+grid[r+i][c+1]+grid[r+i][c+2] !== rSum){
+            return false;
+        }
+        //now move col
+        if(grid[r][c+i]+grid[r+1][c+i]+grid[r+2][c+i] !== rSum){
+            return false;
+        }
+    }
 
-  //diagonal sum
-  if(grid[r][c]+grid[r+1][c+1]+grid[r+2][c+2] !== rSum){
-      return false;
-  }
-  //alerative diagonal sum
-  if(grid[r][c+2]+grid[r+1][c+1]+ grid[r+2][c] !== rSum){
-      return false;
-  }
-  return true;
+    //diagonal sum
+    if(grid[r][c]+grid[r+1][c+1]+grid[r+2][c+2] !== rSum){
+        return false;
+    }
+    //alerative diagonal sum
+    if(grid[r][c+2]+grid[r+1][c+1]+ grid[r+2][c] !== rSum){
+        return false;
+    }
+    return true;
 }
