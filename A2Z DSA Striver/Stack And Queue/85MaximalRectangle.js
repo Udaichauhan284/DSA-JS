d36768bdc327dbb8277a24e96a949ababe698d3a
Udai Chauhan <udaichauhan284@gmail.com>
2026-01-11 22:13:09 +0530

    update the 85 in A2Z Stack and Queue

--- A2Z DSA Striver/Stack And Queue/85MaximalRectangle.js
+++ A2Z DSA Striver/Stack And Queue/85MaximalRectangle.js
@@ -63,3 +63,62 @@ function getNextSmallerRight(heights,n){
 }
 let matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]];
 console.log(maximalRectangle(matrix));
+
+var maximalRectangle = function(matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let currRow = matrix[0];
+    let maxAns = maxHistogram(currRow);
+    for(let i=1; i<m; i++){
+      for(let j=0; j<n; j++){
+        if(matrix[i][j] === '1'){
+          currRow[j]++;
+        }else{
+          currRow[j]=0;
+        }
+      }
+      let currAns = maxHistogram(currRow);
+      maxAns = Math.max(maxAns, currAns);
+    }
+    return maxAns;
+};
+function maxHistogram(nums){
+  let len = nums.length;
+  let NSL = getNextSmallElemLeft(nums);
+  let NSR = getNextSmallElemRight(nums);
+  let maxArea = 0;
+  for(let i=0; i<len; i++){
+    maxArea = Math.max(maxArea, nums[i]*(NSR[i] - NSL[i] -1));
+  }
+  return maxArea;
+}
+function getNextSmallElemLeft(nums){
+  let result = [];
+  let st = [];
+  let len = nums.length;
+  for(let i=0; i<len; i++){
+    let ele = nums[i];
+    while(st.length !== 0 && nums[st[st.length-1]] >= ele){
+      st.pop();
+    }
+    //now put in result,if st.length === 0 -> -1
+    result[i] = (st.length === 0) ? -1 : st[st.length-1];
+    st.push(i);
+  }
+  return result;
+}
+function getNextSmallElemRight(nums){
+  let result = [];
+  let st=[];
+  let len = nums.length;
+  for(let i=len-1; i>=0; i--){
+    let ele = nums[i];
+    while(st.length !== 0 && nums[st[st.length-1]] > ele){
+      st.pop();
+    }
+    //now put in result arr, if you able to found the smalles elem right
+    result[i] = (st.length === 0) ? len : st[st.length-1];
+    st.push(i);
+  }
+  return result;
+}
