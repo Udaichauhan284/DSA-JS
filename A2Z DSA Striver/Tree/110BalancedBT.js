bd506ce36f8fa6e18861f35616a8358f4bf4db99
Udai Chauhan <udaichauhan284@gmail.com>
2026-02-08 20:07:25 +0530

    update the 110 in A2ZDSA Tree section

--- A2Z DSA Striver/Tree/110BalancedBT.js
+++ A2Z DSA Striver/Tree/110BalancedBT.js
@@ -48,3 +48,59 @@ function dfs(root){
 
   return Math.max(leftHeight, rightHeight)+1;
 }
+
+
+
+//FROM 8 FEB 2026, Leetcode POTD
+/*Method 1, in this we can use the recusion, which can tell us
+the height of subtree and also check is left and right is balance
+or not.
+TC: O(n^2), suppose we need to traverse over linear binary tree
+so for sum of height n*(n+1)/2, SC: O(n) for recursion stack space.
+*/
+var isBalanced = function(root) {
+    if(root === null) return true;
+
+    let leftH = height(root.left);
+    let rightH = height(root.right);
+
+    //now check the difference between
+    if(Math.abs(leftH-rightH) > 1) return false;
+
+    //now check if left and right is balance or not
+    return isBalanced(root.left) && isBalanced(root.right);
+};
+function height(node){
+    if(node === null) return 0;
+
+    let lH = height(node.left);
+    let rH = height(node.right);
+
+    //return the maxHeight from that node to its subtree
+    return Math.max(lH,rH)+1;
+}
+
+
+/*Method 2, in this we find the height by going till bottom 
+and check if we get balance or not.
+TC: O(n), SC: O(1) n just for recursion stack 
+*/
+var isBalanced = function(root) {
+    return dfs(root) !== -1;
+};
+function dfs(root){
+    if(root === null) return 0; //balance;
+
+    let lH = dfs(root.left);
+    //now early check
+    if(lH === -1) return -1;
+
+    let rH = dfs(root.right);
+    //now early check
+    if(rH === -1) return -1;
+
+    //now check the balance by checking the abs difference
+    if(Math.abs(lH-rH) > 1) return -1;
+
+    return Math.max(lH, rH)+1;
+}
